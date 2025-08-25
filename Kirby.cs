3034293225230a2bfb9f981342a3d84c92129e81
Vivanto <1425078256@qq.com>
2025-08-25 11:15:43 +0800

    Add Kirby scene

---
+++ Kirby.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class Kirby : RigidBody2D
+{
+	[Export]
+	public float jumpForce = 300f;
+
+	public override void _Process(double delta)
+	{
+		if (Input.IsActionJustPressed("Jump"))
+		{
+			ApplyImpulse(Vector2.Up * jumpForce);
+		}
+	}
+}
